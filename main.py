ff0fae9918b3d2384e3d2500c5e34cff01dc4d5a
tycoder1 <tyty122704@gmail.com>
2020-07-27 17:19:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+total = 0
+def Start(i):
+    a = input('Will you go first or second ')
+    if i == 'first':
+       Add()  
+ 
+def Add(i):
+    b = input('enter 1 or 2 ')
+    if i == '1':
+         return(1 + total)
+    else:
+             return(2 + total)
+             
+Add(i)
+
+
+        
