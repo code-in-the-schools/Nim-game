021965648bdf2f05d0d3c45e92c10987fe54fa99
tycoder1 <tyty122704@gmail.com>
2020-07-28 15:20:25 +0000

    changed total code

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-total = 0
 def Start():
     a = str(input('Will you go first or second '))
     if a == 'first':
        print('Player 1 starts')  
- 
+total = 0
 def Add():
+    total = 0
     b = input('enter 1 or 2 ')
     if b == '1':
-        total + 1
+        total += 1
         print(total)
-        if b == '2':
-             total + 2
+    if b == '2':
+             total += 2
              print(total)
              print('Player 2 turn')
 Start()
